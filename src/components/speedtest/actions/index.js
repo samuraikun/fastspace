1f231629957bbcbc68cefb5b01e14ca716475184
samuraikun <tazitaziawawa@gmail.com>
2018-11-17 23:17:14 +0900

    cosmetic speedtest action

--- src/components/speedtest/actions/index.js
+++ src/components/speedtest/actions/index.js
@@ -1,14 +1,7 @@
 import { fetchSampleData } from '../../../config/mockApi';
 
-const actions = store => ({
-	increment(state) {
-		return { count: state.count+1 };
-	},
-	increment2: ({ count }) => ({ count: count+1 }),
-	incrementAndLog: ({ count }, event) => {
-		console.info(event);
-		return { count: count+1 };
-	},
+const actions = (store) => ({
+	increment: ({ count }) => ({ count: count+1 }),
 	async getUsers(state) {
 		let res = await fetchSampleData();
 		return { users: res };
