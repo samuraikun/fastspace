2542c47572738ecf175da9546f0846ad1298d1d1
samuraikun <tazitaziawawa@gmail.com>
2018-11-17 23:19:22 +0900

    async/await 構文をビルドできるようにする

---
+++ preact.config.js
@@ -0,0 +1,14 @@
+// async/await 構文をビルド可能にするための設定
+// https://github.com/developit/preact-cli/issues/578
+export default (config, env, helpers) => {
+
+	if (env.isProd) {
+
+		// Make async work
+		let babel = config.module.loaders.filter( loader => loader.loader === 'babel-loader')[0].options;
+		// Blacklist regenerator within env preset:
+		babel.presets[0][1].exclude.push('transform-async-to-generator');
+		// Add fast-async
+		babel.plugins.push([require.resolve('fast-async'), { spec: true }]);
+	}
+};
