71a60e0c32c19b931865ce81507d775d7285cd33
samuraikun <tazitaziawawa@gmail.com>
2018-11-17 22:05:22 +0900

    fix mock data

--- src/components/speedtest/sampleData.js
+++ src/components/speedtest/sampleData.js
@@ -1,56 +1,54 @@
-const sampleData = {
-	users: [
-		{
-			id: 0,
-			name: 'Erik Raymond',
-			work: 'ForeTrust',
-			email: 'erik.raymond@foretrust.net',
-			dob: '1941',
-			address: '31 Hill Street',
-			city: 'Fresno',
-			optedin: true
-		},
-		{
-			id: 1,
-			name: 'Cory Risley',
-			work: 'Zatheon',
-			email: 'cory.risley@zatheon.gov',
-			dob: '1976',
-			address: '54 Harvard Avenue',
-			city: 'Baltimore',
-			optedin: false
-		},
-		{
-			id: 2,
-			name: 'Lynetta Pullman',
-			work: 'DuoServe',
-			email: 'lynetta.pullman@duoserve.com',
-			dob: '1911',
-			address: '71 Windsor Avenue',
-			city: 'Canterbury',
-			optedin: true
-		},
-		{
-			id: 3,
-			name: 'Travis Magby',
-			work: 'Solexis',
-			email: 'travis.magby@solexis.club',
-			dob: '1982',
-			address: '93 Fuller Road',
-			city: 'Richmond',
-			optedin: true
-		},
-		{
-			id: 4,
-			name: 'Jamie White',
-			work: 'Dynalab',
-			email: 'jamie.white@dynalab.xyz',
-			dob: '1901',
-			address: '30 Haredale Road',
-			city: 'Chicago',
-			optedin: true
-		}
-	]
-};
+const sampleData = [
+	{
+		id: 0,
+		name: 'Erik Raymond',
+		work: 'ForeTrust',
+		email: 'erik.raymond@foretrust.net',
+		dob: '1941',
+		address: '31 Hill Street',
+		city: 'Fresno',
+		optedin: true
+	},
+	{
+		id: 1,
+		name: 'Cory Risley',
+		work: 'Zatheon',
+		email: 'cory.risley@zatheon.gov',
+		dob: '1976',
+		address: '54 Harvard Avenue',
+		city: 'Baltimore',
+		optedin: false
+	},
+	{
+		id: 2,
+		name: 'Lynetta Pullman',
+		work: 'DuoServe',
+		email: 'lynetta.pullman@duoserve.com',
+		dob: '1911',
+		address: '71 Windsor Avenue',
+		city: 'Canterbury',
+		optedin: true
+	},
+	{
+		id: 3,
+		name: 'Travis Magby',
+		work: 'Solexis',
+		email: 'travis.magby@solexis.club',
+		dob: '1982',
+		address: '93 Fuller Road',
+		city: 'Richmond',
+		optedin: true
+	},
+	{
+		id: 4,
+		name: 'Jamie White',
+		work: 'Dynalab',
+		email: 'jamie.white@dynalab.xyz',
+		dob: '1901',
+		address: '30 Haredale Road',
+		city: 'Chicago',
+		optedin: true
+	}
+];
 
 export default sampleData;
